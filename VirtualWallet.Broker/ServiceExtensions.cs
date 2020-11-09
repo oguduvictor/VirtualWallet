499f2ed05cf2bc072d3525c4c887b706e08bda5f
Victor Ogudu <victor.ogudu@gmail.com>
2020-11-09 17:42:12 +0100

    Added ServiceExtension for Broker

---
+++ VirtualWallet.Broker/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using VirtualWallet.Broker.Bus;
+
+namespace VirtualWallet.Broker
+{
+    public static class ServiceExtensions
+    {
+        public static void AddBrokerServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+        }
+    }
+}
